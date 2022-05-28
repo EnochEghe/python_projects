7b44d4606cf34d94ea5f68c22b6ac812f42f434e
enoch <enocheghe@gmail.com>
2022-05-28 18:32:54 +0100

    adding python files

---
+++ calculator.py
@@ -0,0 +1,18 @@
+print ("Please press enter")
+operation = input('''
+''') 
+
+number_1 = int(input('Enter 1st number and press enter: '))
+number_2 = int(input('Enter your 2nd number and press enter: '))
+
+print('{} + {} = '.format(number_1, number_2))
+print(number_1 + number_2)
+
+print('{} - {} = '.format(number_1, number_2))
+print(number_1 - number_2)
+
+print('{} * {} = '.format(number_1, number_2))
+print(number_1 * number_2)
+
+print('{} / {} = '.format(number_1, number_2))
+print(number_1 / number_2)
